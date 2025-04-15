e4586d26b277e36e91e58b7aa34c2eb1561a5200
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-04-15 14:57:36 +0300

    Onboard redis for refresh tokens

--- src/main/java/com/biezbardis/thoughtyusers/entity/RefreshToken.java
+++ src/main/java/com/biezbardis/thoughtyusers/entity/RefreshToken.java
@@ -1,45 +1,36 @@
 package com.biezbardis.thoughtyusers.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.Date;
 import java.util.UUID;
 
-@Entity
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "refresh_tokens")
+@RedisHash("refresh_tokens")
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
     private UUID id;
 
-    @Column(nullable = false)
     private String username;
 
-    @Column(nullable = false)
     private String issuer;
 
-    @Column(nullable = false)
     private String audience;
 
-    @Column(nullable = false)
     private Date issuedAt;
 
-    @Column(nullable = false)
     private Date expiration;
 }
