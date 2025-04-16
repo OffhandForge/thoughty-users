ca74219f97c203f34a03406d55641d55cfb9311c
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-04-16 18:53:17 +0300

    Make refresh token grants by http only cookie

--- src/main/java/com/biezbardis/thoughtyusers/controller/RefreshTokenController.java
+++ src/main/java/com/biezbardis/thoughtyusers/controller/RefreshTokenController.java
@@ -1,19 +1,22 @@
 package com.biezbardis.thoughtyusers.controller;
 
+import com.biezbardis.thoughtyusers.ThoughtyUsersApplication;
+import com.biezbardis.thoughtyusers.dto.AuthenticationResponse;
 import com.biezbardis.thoughtyusers.dto.RefreshTokenRequest;
-import com.biezbardis.thoughtyusers.dto.RefreshTokenResponse;
 import com.biezbardis.thoughtyusers.service.RefreshTokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("${api.base-path}")
+@RequestMapping(ThoughtyUsersApplication.API_BASE_PATH)
 @RequiredArgsConstructor
 @Tag(name = "Access token renewal")
 public class RefreshTokenController {
@@ -21,7 +24,9 @@ public class RefreshTokenController {
 
     @Operation(summary = "User is refreshing access token")
     @PostMapping("/refresh-token")
-    public RefreshTokenResponse refreshToken(@RequestBody @Valid RefreshTokenRequest request) {
-        return refreshService.refreshAccessToken(request);
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthenticationResponse refreshToken(@RequestBody @Valid RefreshTokenRequest request) {
+        String accessToken = refreshService.refreshAccessToken(request);
+        return new AuthenticationResponse(accessToken);
     }
 }
