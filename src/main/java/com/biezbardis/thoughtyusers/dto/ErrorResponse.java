a65040c1bb2b5e0d7590195cbb512660d71a3ba3
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-08-24 16:59:22 +0300

    Change timestamp format

--- src/main/java/com/biezbardis/thoughtyusers/dto/ErrorResponse.java
+++ src/main/java/com/biezbardis/thoughtyusers/dto/ErrorResponse.java
@@ -1,9 +1,29 @@
 package com.biezbardis.thoughtyusers.dto;
 
-import java.time.LocalDateTime;
+import lombok.Data;
 
-public record ErrorResponse(String timestamp, int status, String error, Object message, String path) {
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+public final class ErrorResponse {
+    private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+    private final String timestamp;
+    private final int status;
+    private final String error;
+    private final Object message;
+    private final String path;
+
+    public ErrorResponse(String timestamp, int status, String error, Object message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
     public ErrorResponse(int status, String error, Object message, String path) {
-        this(LocalDateTime.now().toString(), status, error, message, path);
+        this(ZonedDateTime.now().format(FORMATTER), status, error, message, path);
     }
 }
