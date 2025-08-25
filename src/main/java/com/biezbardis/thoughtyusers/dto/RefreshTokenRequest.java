99769e5bbac01c1148ea2a3bee1505cd6b7ab92a
Alexander Biezbardis <mr.biezbardis@gmail.com>
2025-08-25 11:04:55 +0300

    Remove redundant pattern cause it is not UUID

--- src/main/java/com/biezbardis/thoughtyusers/dto/RefreshTokenRequest.java
+++ src/main/java/com/biezbardis/thoughtyusers/dto/RefreshTokenRequest.java
@@ -2,7 +2,6 @@ package com.biezbardis.thoughtyusers.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -11,7 +10,5 @@ public class RefreshTokenRequest {
 
     @Schema(description = "Expired access token", example = "eyJhxxx...")
     @NotBlank(message = "Access token cannot be empty")
-    @Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$",
-            message = "Invalid token format")
     private String accessToken;
 }
